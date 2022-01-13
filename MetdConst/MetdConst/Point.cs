dfe7022764744a5410f3e20175bf56b4fc314ad1
iuliki <iulia.bulugu@emanuel.ro>
2022-01-13 20:30:33 +0200

    first

---
+++ MetdConst/MetdConst/Point.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MetdConst
+{
+    class Point
+    {
+        public int x { get; set; }
+        public int y { get; set; }
+
+        public void Print()
+        {
+
+        }
+
+
+    }
+}
